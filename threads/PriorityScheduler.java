58b837ad3e29c7a1a8ec54a25a44f2b303f3c209
Sang <Sang@Sang.local>
2013-02-24 21:00:49 -0800

    #1 #5 doing

--- threads/PriorityScheduler.java
+++ threads/PriorityScheduler.java
@@ -200,7 +200,7 @@ public class PriorityScheduler extends Scheduler {
 		public void acquire(PriorityQueue pq) {
 			//Adjust the state of prevHolder of pq
 			ThreadState prevHolder = pq.holder;
-			if (prevHolder.pqHave != null)
+			if (prevHolder != null)
 				prevHolder.pqHave.remove(pq);
 			
 			//Set this ThreadState to the holder of pq
