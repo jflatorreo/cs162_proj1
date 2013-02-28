537d574f8ce9177509a6eb60eb7392bb2ef12dd2
Jeff Crane <jeff_crane@berkeley.edu>
2013-02-27 21:00:15 -0800

    Infinite Wait Fix

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -44,6 +44,7 @@ public class Communicator {
     	//is waiting for a receiver to return it then
     	//put this sender on the waitQueue
     	while(liveReceiver == null) {
+    		waitingReceivers.wake();
     		waitingSenders.sleep();
     	}
     	/** There is no thread waiting to send this value
